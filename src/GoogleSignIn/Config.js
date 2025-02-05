e9cc8dd5610a18ed5da2f44f188a3e79554dc5c3
rinki <rinkikhandal2000@gmail.com>
2025-02-05 13:08:29 +0530

    .env set name changed

--- src/GoogleSignIn/Config.js
+++ src/GoogleSignIn/Config.js
@@ -2,8 +2,8 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_APP_FIREBASE_AUTHDOMAIN,
+  apiKey: import.meta.env.VITE_APP_FIREBASE_API_K,
+  authDomain: import.meta.env.VITE_APP_FIREBASE_ADOMAIN,
   projectId: import.meta.env.VITE_APP_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_APP_FIREBASE_MESSAGING_SENDER_ID,
